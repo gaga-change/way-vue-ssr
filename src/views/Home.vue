0255979796c989e39f58da4e85946368d90f50aa
严俊东 <1172482914@qq.com>
2018-07-28 10:38:46 +0800

    初始化

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        {{a}}
+    </div>
+</template>
+<script>
+export default {
+    data () {
+        return {
+            a: 'gaga'
+        }
+    },
+    methods: {
+        title () {
+            return this.item.title
+        }
+    }
+}
+</script>
+
